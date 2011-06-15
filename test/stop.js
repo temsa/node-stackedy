d1fa7cf42e6f81b54967beda85204b9815f777d3
James Halliday <mail@substack.net>
2011-06-15 14:24:18 -0700

    failing wait defun test

--- test/stop.js
+++ test/stop.js
@@ -5,7 +5,8 @@ var fs = require('fs');
 var src = {
     interval : fs.readFileSync(__dirname + '/stop/interval.js'),
     timeout : fs.readFileSync(__dirname + '/stop/timeout.js'),
-    wait : fs.readFileSync(__dirname + '/stop/wait.js')
+    wait_function : fs.readFileSync(__dirname + '/stop/wait_function.js'),
+    wait_defun : fs.readFileSync(__dirname + '/stop/wait_defun.js')
 };
 
 exports.interval = function () {
@@ -34,28 +35,30 @@ exports.timeout = function () {
 };
 
 exports.wait = function () {
-    var iv = null;
-    var context = {
-        wait : function (cb) {
-            assert.ok(!iv);
-            iv = setInterval(function () {
-                cb();
-            }, 50);
-        },
-        console : console,
-        exports : {}
-    };
-    
-    var stack = stackedy(src.wait).run(context);
-    
-    setTimeout(function () {
-        stack.stop();
-        assert.equal(context.exports.times, 5);
-    }, 300);
-    
-    setTimeout(function () {
-        assert.equal(context.exports.times, 5);
-        clearInterval(iv);
-    }, 500);
+    [ 'function', 'defun' ].forEach(function (name) {
+        var iv = null;
+        var context = {
+            wait : function (cb) {
+                assert.ok(!iv);
+                iv = setInterval(function () {
+                    cb();
+                }, 50);
+            },
+            console : console,
+            exports : {}
+        };
+        
+        var stack = stackedy(src['wait_' + name]).run(context);
+        
+        setTimeout(function () {
+            stack.stop();
+            assert.equal(context.exports.times, 5);
+        }, 300);
+        
+        setTimeout(function () {
+            assert.equal(context.exports.times, 5);
+            clearInterval(iv);
+        }, 500);
+    });
 };
 
