129e513c87147e2ba2fd7f9a5f16b5757d354aa2
James Halliday <mail@substack.net>
2011-09-18 21:23:14 -0700

    fixed simple example

--- example/simple/run.js
+++ example/simple/run.js
@@ -4,13 +4,14 @@ var fs = require('fs');
 var src = fs.readFileSync(__dirname + '/src.js');
 var stack = stackedy(src, { filename : 'stax.js' }).run();
 
-stack.on('error', function (err) {
-    console.log('Error: ' + err.message);
+stack.on('error', function (err, c) {
+    stack.stop();
+    console.log('Error: ' + err);
     
-    var c = err.current;
-    console.log('  in ' + c.filename + ' at line ' + c.start.line);
+    var cur = c.current;
+    console.log('  in ' + cur.filename + ' at line ' + cur.start.line);
     
-    err.stack.forEach(function (s) {
+    c.stack.forEach(function (s) {
         console.log('  in ' + s.filename + ', '
             + s.functionName + '() at line ' + s.start.line
         );
