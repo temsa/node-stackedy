716e0c48d0697c452ebaeffdca5d21f58064bbd5
James Halliday <mail@substack.net>
2011-09-18 21:22:16 -0700

    fixed pretty example

--- example/pretty/run.js
+++ example/pretty/run.js
@@ -1,17 +1,19 @@
 var stackedy = require('stackedy');
 var fs = require('fs');
 
-var src = fs.readFileSync(__dirname + '/src.js');
+var src = fs.readFileSync(__dirname + '/src.js', 'utf8');
 var stack = stackedy(src, { filename : 'stax.js' }).run();
 
-stack.on('error', function (err) {
-    console.log('Error: ' + err.message + '\n');
+stack.on('error', function (err, c) {
+    console.log('Error: ' + err + '\n');
     
-    var c = err.current;
-    console.log('  in ' + c.filename + ' at ' + getRange(c) + ':');
-    console.log(getCode(c, 4));
+    var cur = c.current;
+    console.log('  in ' + cur.filename + ' at ' + getRange(cur) + ':');
+    console.log(getCode(cur, 4));
     
-    err.stack.forEach(function (s) {
+    c.stack.forEach(function (s) {
+        stack.stop();
+        
         if (s.name === 'call') {
             
             var range = getRange(s);
@@ -33,16 +35,17 @@ function getRange (s) {
 }
 
 function getCode (s, ix) {
+    var lines = src.split('\n').slice(s.start.line, s.end.line + 1);
     var indent = Array(ix + 1).join(' ');
-    var code = indent + s.lines[0].replace(/^\s+/, '');
+    var code = indent + lines[0].replace(/^\s+/, '');
     
-    if (s.lines.length > 2) code += ' ... ';
-    if (s.lines.length > 1) {
-        code += s.lines.slice(-1)[0].replace(/^\s+/, '');
+    if (lines.length > 2) code += ' ... ';
+    if (lines.length > 1) {
+        code += lines.slice(-1)[0].replace(/^\s+/, '');
     }
     
     if (s.start.line === s.end.line) {
-        var pad = ix + s.start.col - s.lines[0].match(/^(\s*)/)[1].length;
+        var pad = ix + s.start.col - lines[0].match(/^(\s*)/)[1].length;
         code += '\n' + Array(pad + 1).join(' ')
             + Array(s.end.col - s.start.col + 2).join('~');
     }
