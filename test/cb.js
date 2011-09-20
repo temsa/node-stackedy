e8c330247498580f341091ba5ef655afe0305e6c
James Halliday <mail@substack.net>
2011-09-20 05:50:30 -0700

    cb test fails at checking for current

--- test/cb.js
+++ test/cb.js
@@ -5,14 +5,18 @@ var fs = require('fs');
 var src = fs.readFileSync(__dirname + '/sources/cb.js', 'utf8');
 
 test('cb', function (t) {
-    t.plan(5);
+    t.plan(7);
     var stack = stackedy(src).run({
         fn : function (name, cb) {
             t.equal(name, 'quoymielle')
             cb({
                 ok : t.ok.bind(t),
                 equal : t.equal.bind(t),
-                end : t.end.bind(t),
+                end : function () {
+                    t.ok(stack.current);
+                    t.equal(stack.current.source(), 't.end();');
+                    t.end();
+                },
                 wuxxet : function (x, cb) {
                     t.equal(x, 'flibble');
                     cb('kallisti');
